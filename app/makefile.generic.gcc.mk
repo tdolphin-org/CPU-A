3231129b9ec8b28fd757c64a97cafdbb0acf2443
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-04 19:30:22 +0100

    remove AOS::Identify (moved to AmigaOS.cpp.wrapper project), update makefiles

--- app/makefile.generic.gcc.mk
+++ app/makefile.generic.gcc.mk
@@ -9,6 +9,7 @@
 #
 # cross compilation setup from -> https://github.com/bebbo/amiga-gcc
 #
+#  	  requires: AmigaOS C++ wrapper classes (https://github.com/tdolphin-org/AmigaOS.cpp.wrapper)
 #  	  requires: AmigaOS MUI C++ wrapper classes (https://github.com/tdolphin-org/AmigaOS.MUI.cpp.wrapper)
 #
 
@@ -19,7 +20,8 @@ MORE_LFLAGS_X = $(shell echo $(MORE_LFLAGS) | tr ',' ' ')
 # trace/debug flags
 DEBUG_FLAGS =
 
-CPP_FLAGS = $(DEBUG_FLAGS) $(MORE_CPP_FLAGS_X) -std=c++17 -Isrc -I${MUICPP_PATH}/wrappers/src -I${MUICPP_PATH}/components/src -O1
+CPP_FLAGS = $(DEBUG_FLAGS) $(MORE_CPP_FLAGS_X) -std=c++17 -O1\
+	-Isrc -I${AOSCPP_PATH}/wrappers/src -I${MUICPP_PATH}/wrappers/src -I${MUICPP_PATH}/components/src
 LFLAGS = -L${MUICPP_PATH}/wrappers/lib/$(SUB_BUILD_PATH) -lMUIcpp $(MORE_LFLAGS_X) -lstdc++ -noixemul
 
 dir_guard = mkdir -p $(@D)
@@ -27,6 +29,11 @@ dir_guard = mkdir -p $(@D)
 BINPATH = out/$(SUB_BUILD_PATH)
 
 include makefile.gen.version.mk
+
+AOS_WRAPPER_PATH = ${AOSCPP_PATH}/wrappers
+AOS_WRAPPER_MODULES = AOS/Identify
+AOS_WRAPPER_SRC_DIRS = $(addprefix $(AOS_WRAPPER_PATH)/src/,$(AOS_WRAPPER_MODULES))
+AOS_WRAPPER_SRCS = $(foreach sdir,$(AOS_WRAPPER_SRC_DIRS),$(wildcard $(sdir)/*.cpp))
 
 MUI_COMPONENTS_PATH = ${MUICPP_PATH}/components
 MUI_COMPONENTS_MODULES = Components/Core Components/MCC Components/MCC/Core
@@ -39,6 +46,7 @@ MODULES = $(MODULES_COMPONENTS) FileResources TextResources DataInfo AOS/Identif
 SRC_DIRS = src $(addprefix src/,$(MODULES))
 SRCS = $(foreach sdir,$(SRC_DIRS),$(wildcard $(sdir)/*.cpp))
 OBJS = $(patsubst src/%.cpp,obj/$(SUB_BUILD_PATH)/%.o,$(SRCS))\
+	$(patsubst $(AOS_WRAPPER_PATH)/src/%.cpp,$(AOS_WRAPPER_PATH)/obj/$(SUB_BUILD_PATH)/%.o,$(AOS_WRAPPER_SRCS))\
 	$(patsubst $(MUI_COMPONENTS_PATH)/src/%.cpp,$(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/%.o,$(MUI_COMPONENTS_SRCS))
 
 HEADERS = src/ProgDefines.hpp
@@ -57,13 +65,10 @@ obj/$(SUB_BUILD_PATH)/%.o: src/%.cpp src/%.hpp $(HEADERS)
 	$(dir_guard)
 	$(CPPC) $(CPP_FLAGS) -c $< -o $@
 
-$(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/%.o: $(MUI_COMPONENTS_PATH)/src/%.cpp $(MUI_COMPONENTS_PATH)/src/%.hpp
+$(AOS_WRAPPER_PATH)/obj/$(SUB_BUILD_PATH)/%.o: $(AOS_WRAPPER_PATH)/src/%.cpp $(AOS_WRAPPER_PATH)/src/%.hpp
 	$(dir_guard)
 	$(CPPC) $(CPP_FLAGS) -c $< -o $@
 
-clean :
-	rm -f $(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/*.o
-	rm -f obj/$(SUB_BUILD_PATH)/*.o
-	rm -f -R out/$(SUB_BUILD_PATH)/*
-
-rebuild : clean all
+$(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/%.o: $(MUI_COMPONENTS_PATH)/src/%.cpp $(MUI_COMPONENTS_PATH)/src/%.hpp
+	$(dir_guard)
+	$(CPPC) $(CPP_FLAGS) -c $< -o $@
