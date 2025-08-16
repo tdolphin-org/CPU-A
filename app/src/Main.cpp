53bc2fc0c3dd5f68542f4f2fba72cb70bbffdc53
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-08-16 12:49:09 +0200

    add stack size checks in Main.cpp

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -8,10 +8,45 @@
 
 #include "App.hpp"
 
-#include <dos/dos.h>
-#include <iostream>
+#include "AOS/Dos/Library.hpp"
+
+#include "amiga_std_light/iostream.hpp"
+#include <proto/exec.h>
+#include <proto/dos.h>
 
 using namespace std;
+
+long minStackSize = 32000;
+
+void check_stack_size(void) __attribute__((constructor));
+
+void check_stack_size(void)
+{
+    auto process = AOS::Dos::Library::FindProcess();
+    if (!process)
+    {
+        Printf("Failed to find current process!\n");
+        exit(RETURN_FAIL);
+    }
+
+    if (process->pr_CLI) // run from CLI
+    {
+        auto cli = (struct CommandLineInterface *)BADDR(process->pr_CLI);
+        if (cli->cli_DefaultStack * 4 < minStackSize)
+        {
+            Printf("Stack size is too small! Required 32000\n");
+            exit(RETURN_FAIL);
+        }
+    }
+    else
+    {
+        if (process->pr_StackSize < minStackSize)
+        {
+            Printf("Stack size is too small! Required 32000\n");
+            exit(RETURN_FAIL);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
