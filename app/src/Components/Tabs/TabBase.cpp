71579c1e53723d63b405dfebc0d185d41bc805ac
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-18 19:35:25 +0200

    update TabBase component

--- app/src/Components/Tabs/TabBase.cpp
+++ app/src/Components/Tabs/TabBase.cpp
@@ -10,15 +10,11 @@ namespace Components
 {
     MUI::Text TabBase::LabelText(const std::string &label)
     {
-        return MUI::TextBuilder().tagContents(label).tagFrame(MUI::Frame::String).tagPhantomHoriz(true).tagWeight(0).object();
+        return MUI::TextBuilder().tagContents(label).tagFrame(MUI::Frame::String).tagFramePhantomHoriz(true).tagWeight(0).object();
     }
 
     MUI::Text TabBase::ValueText(const std::string &help, const std::string &defaultValue)
     {
-        return MUI::TextBuilder()
-            .tagFrame(MUI::Frame::String)
-            .tagContents(defaultValue)
-            .tagShortHelp(help)
-            .object();
+        return MUI::TextBuilder().tagFrame(MUI::Frame::String).tagContents(defaultValue).tagShortHelp(help).object();
     }
 }
